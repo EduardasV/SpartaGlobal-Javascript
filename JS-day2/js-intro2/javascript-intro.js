cb8eaec20d47c220c0c062b343b448a02aa11472
Eduardas Verba <eduardas.v@hotmail.com>
2018-08-07 11:11:17 +0100

    day 2 initial commit

--- JS-day2/js-intro2/javascript-intro.js
+++ JS-day2/js-intro2/javascript-intro.js
@@ -1,6 +1,17 @@
+/* ------------------ feedback ------------------
+
+ * don't edit presenting code before you present
+ * if you use short variable names that don't make sense comment it out
+ * have a stage repository, which you use to present so if you do make changes to code it won't break your presentation code
+ * clear variable names, so if you look at it you know exactly what it is
+
+ ------------------ feedback end ------------------ */
+
 // Richards demo - something you should never use.
 var num1 = 5;
 var operator = "+";
 var num2 = 10;
 
 eval(num1 + operator + num2);
+
+// ------------------ FUNCTIONS INTRO ------------------
