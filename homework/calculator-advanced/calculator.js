15ea4248a187878770a79a3d67e5e8ad32d4c700
Eduardas Verba <eduardas.v@hotmail.com>
2018-08-07 21:25:12 +0100

    BMI calculator

--- homework/calculator-advanced/calculator.js
+++ homework/calculator-advanced/calculator.js
@@ -1,3 +1,21 @@
+
+
+function BMI(){
+  var metric_or_imperial_input = prompt("(1)Metric or (2)Imperial");
+  switch (metric_or_imperial_input) {
+    case "1":
+      var first_number_input = parseFloat(prompt("Input your weight in kilograms: "));
+      var second_number_input = parseFloat(prompt("Input your height in centimeters: "))/100;
+      alert("Your BMI is: " + ((first_number_input/Math.pow(second_number_input,2))));
+      break;
+    case "2":
+      var first_number_input = parseFloat(prompt("Input your weight in pounds: "));
+      var second_number_input = parseFloat(prompt("Input your height in inches: "));
+      alert("Your BMI is: " + (((first_number_input*703)/Math.pow(second_number_input,2))));
+      break;
+    default:
+  }
+}
 function Calculator(){
   var first_number_input = parseFloat(prompt("Enter your first number: "));
   var operation_input = prompt("Enter your operation for: " + first_number_input);
